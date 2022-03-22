b043237b0bb5a57296f7d1db3c88c088abc3291c
Shumon Sharif <shumon.sharif@gmail.com>
2022-03-22 18:00:25 -0400

    Test lgtm RCE flagging

--- src/main/java/org/example/LookGoodToMe.java
+++ src/main/java/org/example/LookGoodToMe.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 @RestController
 @RequestMapping("/lgtm")
 public class LookGoodToMe {
@@ -16,7 +20,36 @@ public class LookGoodToMe {
     @GetMapping("/hello")
     public String execute(@RequestParam(defaultValue = "world") String name) {
         logger.info(name);
+        commandExecutor(name);
         return "Hello " + name + "!";
+    }
+
+    // Hopefully flag RCE
+    public String commandExecutor(String cmd) {
+        Runtime run = Runtime.getRuntime();
+        StringBuilder sb = new StringBuilder();
+
+        try {
+            Process p = run.exec(cmd);
+            BufferedInputStream in = new BufferedInputStream(p.getInputStream());
+            BufferedReader inBr = new BufferedReader(new InputStreamReader(in));
+            String tmpStr;
+
+            while ((tmpStr = inBr.readLine()) != null) {
+                sb.append(tmpStr);
+            }
+
+            if (p.waitFor() != 0) {
+                if (p.exitValue() == 1)
+                    return "Command exec failed!!";
+            }
+
+            inBr.close();
+            in.close();
+        } catch (Exception e) {
+            return e.toString();
+        }
+        return sb.toString();
     }
 
 }
